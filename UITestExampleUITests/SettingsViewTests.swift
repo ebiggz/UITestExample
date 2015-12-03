fe2b69026f377a98723b010970458b48049a27ab
ebiggz <erikbigler@gmail.com>
2015-12-03 14:53:19 -0800

    Picker tests and cleanup

--- UITestExampleUITests/SettingsViewTests.swift
+++ UITestExampleUITests/SettingsViewTests.swift
@@ -9,15 +9,15 @@
 import XCTest
 
 class SettingsViewTests: XCTestCase {
+
+    let app = XCUIApplication()
     
     override func setUp() {
         super.setUp()
-        
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
         continueAfterFailure = false
 
-        XCUIApplication().launch()
+        app.launch()
 
         UITestHelper.navigateToView(.Settings)
 
@@ -30,25 +30,40 @@ class SettingsViewTests: XCTestCase {
 
     func testRenameTeam() {
 
-        let app = XCUIApplication()
         let nameField = app.tables.cells.textFields["Team Name Field"]
 
         nameField.clearAndTypeText("Denver Broncos")
         app.typeText("\n")
-
         UITestHelper.navigateToView(.Home)
 
         XCTAssert(app.navigationBars["Denver Broncos"].exists)
-
     }
 
     func testUpdateTeamSkill() {
 
-        let app = XCUIApplication()
         let slider = app.tables.cells.sliders.element
 
+        //This isnt working for some reason
         slider.adjustToNormalizedSliderPosition(0.9)
-
     }
 
+    func testPickPreferredDefence() {
+        
+        let defencePicker = app.pickerWheels.elementBoundByIndex(1)
+        let defenceLabel = app.staticTexts["Selected Defence Display"]
+
+        defencePicker.adjustToPickerWheelValue("Nickel")
+
+        XCTAssertEqual(defenceLabel.label, "Nickel")
+    }
+
+    func testPickPreferredOffence() {
+
+        let offencePicker = app.pickerWheels.elementBoundByIndex(0)
+        let offenceLabel = app.staticTexts["Selected Offence Display"]
+
+        offencePicker.adjustToPickerWheelValue("IFormation")
+
+        XCTAssertEqual(offenceLabel.label, "IFormation")
+    }
 }
